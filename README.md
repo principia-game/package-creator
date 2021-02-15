a5f27491c34b31c3bf4b8d43e3c23da6832b73c0
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-02-15 13:40:02 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Package Creator
+Quick and dirty Python script to generate a playable package file from a JSON file.
+
+An example.json file is provided as an example JSON file, which you can edit to suit your needs.
---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+import struct
+import sys
+from pathlib import Path
+
+def u8(data):
+	if not 0 <= data <= 255:
+		print("u8 out of range: %s" % data, "INFO")
+		data = 0
+	return struct.pack(">B", data)
+
+def u32(data):
+	if not 0 <= data <= 4294967295:
+		print("u32 out of range: %s" % data, "INFO")
+		data = 0
+	return struct.pack("<I", data)
+
+def main(filename, outfile):
+	data = json.loads(open(Path(filename), "r").read())
+
+	with open(outfile, 'wb+') as f:
+		f.write(u8(3))									# version
+		f.write(u32(data['unknown_e621ad']))			# unknown_e621ad
+		f.write(data['name'].encode())					# name
+		f.write(b'\x00' * (255 - len(data['name'])))	# 	(name padding)
+		f.write(u8(data['levels_unlocked']))			# levels_unlocked
+		f.write(u8(data['unknown_798b9a']))				# unknown_798b9a
+		f.write(u8(data['unknown_8fe981']))				# unknown_8fe981
+		f.write(u8(len(data['levels'])))				# levels
+		for level in data['levels']:
+			f.write(u32(level))							# level_id
+
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print("Usage: python main.py [input json file] [output ppkg file]")
+	else:
+		main(sys.argv[1], sys.argv[2])
